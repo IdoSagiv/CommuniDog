04d16d0640b822ab4994c8cac55f685b3f7d5130
hadas241 <hadas.aizik@mail.huji.acil>
2021-06-23 23:54:51 +0300

    updated the about window - design changes and added link to git

--- app/src/main/java/communi/dog/aplicatiion/AboutPage.java
+++ app/src/main/java/communi/dog/aplicatiion/AboutPage.java
@@ -2,7 +2,9 @@ package communi.dog.aplicatiion;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.util.DisplayMetrics;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
@@ -12,13 +14,20 @@ public class AboutPage extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_section);
+
+        setupHyperLink();
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
 
         int width = dm.widthPixels;
         int height = dm.heightPixels;
 
-        getWindow().setLayout((int) (width*.8), (int )(height* .6));
+        getWindow().setLayout((int) (width*.7), (int )(height*.5));
 
+    }
+
+    private void setupHyperLink() {
+        TextView linkToGitView = findViewById(R.id.about_info);
+        linkToGitView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
