dc1f8ed821c6bdc2b8a0d2541d3795ada456eb8a
Ido Sagiv <ido10796@gmail.com>
2021-06-24 13:19:40 +0300

    small indentations and rephrasing fixes

--- app/src/main/java/communi/dog/aplicatiion/AboutPage.java
+++ app/src/main/java/communi/dog/aplicatiion/AboutPage.java
@@ -22,7 +22,7 @@ public class AboutPage extends Activity {
         int width = dm.widthPixels;
         int height = dm.heightPixels;
 
-        getWindow().setLayout((int) (width*.7), (int )(height*.5));
+        getWindow().setLayout((int) (width * .7), (int) (height * .5));
 
     }
 
