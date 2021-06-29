4cffebaf8bc5b0965a99608ac271a68d5f727846
Ido Sagiv <ido10796@gmail.com>
2021-06-29 16:37:12 +0300

    fix build issues with old tests

--- app/src/test/java/communi/dog/aplicatiion/LoginActivityTest.java
+++ app/src/test/java/communi/dog/aplicatiion/LoginActivityTest.java
@@ -22,15 +22,11 @@ public class LoginActivityTest extends TestCase {
         LoginActivity loginActivity = Robolectric.buildActivity(LoginActivity.class).create().visible().get();
 
         Button login_button = loginActivity.findViewById(R.id.login_button);
-        EditText id = loginActivity.findViewById(R.id.input_id_register);
-        EditText emailAddress = loginActivity.findViewById(R.id.input_email_register);
-        EditText pass1 = loginActivity.findViewById(R.id.input_pass_reg);
-        EditText pass2 = loginActivity.findViewById(R.id.input_repass_reg);
+        EditText emailAddress = loginActivity.findViewById(R.id.input_email_login);
+        EditText pass = loginActivity.findViewById(R.id.user_password);
 
-        id.setText("123456789");
         emailAddress.setText("Test@gmail.com");
-        pass1.setText("111111111");
-        pass2.setText("111111111");
+        pass.setText("111111111");
 
         assertTrue(login_button.isEnabled());
     }
@@ -43,13 +39,13 @@ public class LoginActivityTest extends TestCase {
 
         assertFalse(login_button.isEnabled());
 
-        EditText idEditText = loginActivity.findViewById(R.id.input_email_login);
+        EditText emailAddress = loginActivity.findViewById(R.id.input_email_login);
         EditText userPassword = loginActivity.findViewById(R.id.user_password);
 
-        idEditText.setText("123456789");
+        emailAddress.setText("123456789");
         assertFalse(login_button.isEnabled());
 
-        idEditText.setText("");
+        emailAddress.setText("");
         userPassword.setText("123456789");
         assertFalse(login_button.isEnabled());
     }
@@ -62,10 +58,10 @@ public class LoginActivityTest extends TestCase {
 
         assertFalse(login_button.isEnabled());
 
-        EditText idEditText = loginActivity.findViewById(R.id.input_email_login);
+        EditText emailAddress = loginActivity.findViewById(R.id.input_email_login);
         EditText userPassword = loginActivity.findViewById(R.id.user_password);
 
-        idEditText.setText("123456789");
+        emailAddress.setText("123456789");
         userPassword.setText("123456789");
 
         assertTrue(login_button.isEnabled());
